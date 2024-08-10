f9532a357abdd5f0b07b285bdba0063c7470171b
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-08-10 18:17:53 +0200

    Improve meteor_big.gd

--- scenes/meteors/meteor_big.gd
+++ scenes/meteors/meteor_big.gd
@@ -7,18 +7,21 @@ func _ready():
 
 
 func _on_body_entered(body: Node):
-	var meteor_med_1 = preload("res://scenes/meteors/meteor_med_1.tscn")
-	var meteor_med_2 = preload("res://scenes/meteors/meteor_med_2.tscn")
-	
+	var meteors = [
+		preload("res://scenes/meteors/meteor_med_1.tscn"),
+		preload("res://scenes/meteors/meteor_med_2.tscn")
+	]
+
 	var parent = get_parent()
 	var rng = RandomNumberGenerator.new()
 	
 	for i in 3:
-		var meteor_type = meteor_med_1 if i%2==0 else meteor_med_2
+		var meteor_type = meteors[rng.randi_range(0,1)]
 		var meteor = meteor_type.instantiate()
 		var offset_x = randf_range(-20, 20)
 		var offset_y = randf_range(-20, 20)
 		meteor.global_position = global_position + Vector2(offset_x, offset_y)
+		meteor.linear_velocity = linear_velocity + (meteor.global_position - global_position).normalized() * 25
 		parent.add_child(meteor)
 	
 	queue_free()
