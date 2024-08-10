cef44d4ee77fbc859ba4ce17538eb19dde0efaef
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-08-10 16:16:49 +0200

    Create player movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,4 +8,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("Accelerate"):
+		var force = Vector2.UP.rotated(global_rotation) * 10
+		add_constant_central_force(force)
+	
+	if Input.is_action_just_pressed("Turn Left"):
+		angular_velocity = -5
+	
+	if Input.is_action_just_pressed("Turn Right"):
+		angular_velocity = 5
+	
+	if !Input.is_action_pressed("Turn Left") and !Input.is_action_pressed("Turn Right"):
+		angular_velocity = 0
+	
