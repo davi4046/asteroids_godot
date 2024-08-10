808ee44740f6733fac071cfe8b4819d944dd60c0
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-08-10 17:31:21 +0200

    Create meteor spawn timer

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -5,3 +5,20 @@ func _on_area_2d_body_exited(body):
 	body.set_physics_process(false)
 	body.global_position = Vector2(-body.global_position.x, -body.global_position.y)
 	body.set_physics_process(true)
+
+
+var rng = RandomNumberGenerator.new()
+var timer = Timer.new()
+
+
+func _ready():
+	timer.connect("timeout", _spawn_meteor)	
+	timer.start(rng.randf_range(2, 10))
+	add_child(timer)
+	_spawn_meteor()		
+
+
+func _spawn_meteor():
+	print("Spawn meteor!")	
+	timer.start(rng.randf_range(2, 10))
+	pass
