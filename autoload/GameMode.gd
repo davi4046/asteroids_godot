a45aafd269cee2b015b4b9f31441b7217a8a1b7d
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-08-12 16:38:19 +0200

    Display player lives

--- autoload/GameMode.gd
+++ autoload/GameMode.gd
@@ -2,13 +2,19 @@ extends Node
 
 
 signal score_changed
+signal lives_changed
 
 
-var score = 0:
+var score: int:
 	set = set_score
 
 
+var lives: int:
+	set = set_lives
+
+
 var world: World
+var hud: Hud
 
 
 func _ready():
@@ -16,16 +22,30 @@ func _ready():
 	get_tree().current_scene.add_child(world)
 
 
-func set_score(value):
+func set_score(value: int):
 	score = value
 	emit_signal("score_changed")
 
 
+func set_lives(value: int):
+	lives = value
+	emit_signal("lives_changed")
+
+
 func start_game():
-	var hud = preload("res://ui/hud/hud.tscn").instantiate()
+	score = 0
+	lives = 3
+	hud = preload("res://ui/hud/hud.tscn").instantiate()
 	get_parent().add_child(hud)
 	_spawn_player()
 	world.start_spawning_meteors()
+
+
+func _end_game():
+	hud.queue_free()
+	var start_menu = preload("res://ui/start_menu/start_menu.tscn").instantiate()
+	get_tree().current_scene.add_child(start_menu)
+	world.stop_spawning_meteors()
 
 
 func _spawn_player():
@@ -35,4 +55,8 @@ func _spawn_player():
 
 
 func _on_player_died():
-	_spawn_player()
+	lives -= 1
+	if lives == 0:
+		_end_game()
+	else:
+		_spawn_player()
--- ui/hud/hud.gd
+++ ui/hud/hud.gd
@@ -1,9 +1,30 @@
 extends Control
+class_name Hud
 
 
 func _ready():
 	GameMode.connect("score_changed", _on_score_changed)
+	GameMode.connect("lives_changed", _on_lives_changed)
+	_update_score(GameMode.score)
+	_update_lives(GameMode.lives)
 
 
 func _on_score_changed():
-	$Score.text = str(GameMode.score)
+	_update_score(GameMode.score)
+
+
+func _on_lives_changed():
+	_update_lives(GameMode.lives)
+
+
+func _update_score(score: int):
+	$Score.text = str(score)
+
+
+func _update_lives(lives: int):
+	for child in $LivesContainer.get_children():
+		$LivesContainer.remove_child(child)
+	
+	for i in lives:
+		var player_life = preload("res://ui/hud/player_life/player_life.tscn").instantiate()
+		$LivesContainer.add_child(player_life)
